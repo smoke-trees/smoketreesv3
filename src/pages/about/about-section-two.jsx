e8e7cdae7ecc017022445488b3ae52f14243f8d6
Adarsh singh <adarshmanoj.05.as@gmail.com>
2022-04-24 18:00:50 +0530

    about page complete

--- src/pages/about/about-section-two.jsx
+++ src/pages/about/about-section-two.jsx
@@ -1,10 +1,28 @@
-import React from 'react'
+import React, {useContext} from 'react'
+import {DarkModeContext} from '../../context/darkModeContext';
 import './about.scss'
 
 const AboutSectionTwo = () => {
+      const {darkMode} = useContext(DarkModeContext)
+
   return (
     <div className='about__second-section'>
-      temp
+      <div className='about__second-section__content'>
+        <div className="about__second-section__images">
+          <img src="//unsplash.it/196/303" alt="" className='about__second-section__images__1' />
+          <img src="//unsplash.it/429/663" alt="" className='about__second-section__images__2' />
+          <img src="//unsplash.it/245/378" alt="" className='about__second-section__images__3' />
+        </div>
+        <div className="about__second-section__text-container">
+          <div className="about__second-section__text-container__title">Our Values</div>
+          <div className={`about__second-section__text-container__para ${darkMode ? 'dark-mode__color2' : 'light-mode__color2'}`}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at risus amet, commodo condimentum integer. Something about consultation!
+          </div>
+          <div className={`about__second-section__text-container__para ${darkMode ? 'dark-mode__color2' : 'light-mode__color2'}`}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at risus amet, commodo condimentum integer. Something about consultation!
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
