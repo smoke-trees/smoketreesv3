74a00effa5bfcd156d64111c8a5805be5cc76a15
Adarsh singh <adarsh@smoketrees.in>
2022-09-16 23:35:22 +0530

    feat: dark mode to project details page

--- src/pages/product/product-section-two.jsx
+++ src/pages/product/product-section-two.jsx
@@ -2,9 +2,10 @@ import React, {useContext} from 'react'
 import './product.scss'
 import { DarkModeContext } from '../../context/darkModeContext';
 import GradientImg from '../../components/gradient-img/gradient-img'
+import { Link } from "react-router-dom"
 
 const AboutSectionTwo = () => {
-  const {darkMode, toggleDarkMode} = useContext(DarkModeContext)
+  const {darkMode} = useContext(DarkModeContext)
   return (
     <div className="project__second-section">
       <div className="project__second-section__container">
@@ -29,25 +30,27 @@ const AboutSectionTwo = () => {
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Id dui hendrerit leo dictumst
             feugiat quis arcu integer morbi.
           </div>
-          <div
-            className={`project__second-section__explore ${
-              darkMode ? "dark-mode__color2" : "light-mode__color2"
-            }`}
-          >
-            Explore{" "}
-            <svg
-              width="25"
-              height="25"
-              viewBox="0 0 25 25"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
+          <Link to="/project/projectname">
+            <div
+              className={`project__second-section__explore ${
+                darkMode ? "dark-mode__color2" : "light-mode__color2"
+              }`}
             >
-              <path
-                d="M8.9375 17.2813L13.7187 12.5L8.9375 7.71875L10.4167 6.25L16.6667 12.5L10.4167 18.75L8.9375 17.2813Z"
-                fill={darkMode ? "rgba(255, 255, 255, 0.7)" : "#676767"}
-              />
-            </svg>
-          </div>
+              Explore{" "}
+              <svg
+                width="25"
+                height="25"
+                viewBox="0 0 25 25"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M8.9375 17.2813L13.7187 12.5L8.9375 7.71875L10.4167 6.25L16.6667 12.5L10.4167 18.75L8.9375 17.2813Z"
+                  fill={darkMode ? "rgba(255, 255, 255, 0.7)" : "#676767"}
+                />
+              </svg>
+            </div>
+          </Link>
         </div>
         <div className="project__second-section__img-cont">
           <GradientImg imgUrl="//unsplash.it/577/784" />
