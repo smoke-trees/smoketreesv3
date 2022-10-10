ec1cefa91082d3f5417d25cf9219730ef66b8854
Adarsh singh <adarsh@smoketrees.in>
2022-10-10 15:05:29 +0530

    feat: coming soon page done

---
+++ src/pages/project-name/comming-soon.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Navbar } from '../../components'
+
+const CommingSoon = (props) => {
+  return (
+    <div>
+      <Navbar />
+      <div className="coming-soon__container">
+        <div className="coming-soon__data">
+          <div className="coming-soon__name light-mode__color1">{props.name}</div>
+          <div className="coming-soon__desc light-mode__color1">{props.desc}</div>
+        </div>
+        <div className="coming-soon__text">COMING SOON</div>
+      </div>
+    </div>
+  )
+}
+
+export default CommingSoon
--- src/pages/project-name/project-name.jsx
+++ src/pages/project-name/project-name.jsx
@@ -8,6 +8,7 @@ import MeriBachat from "./meri-bachat"
 import "./project-name.scss"
 import Ombhu from "./ombhu"
 import AltiliumErp from "./altilium-erp"
+import CommingSoon from "./comming-soon"
 
 export const ProjectName = () => {
   const { prodDetsOff, prodDetsUnMnt } = useContext(DarkModeContext)
@@ -39,6 +40,14 @@ export const ProjectName = () => {
 
     case "altilium":
       return <AltiliumErp />
+
+    case "me-time":
+      return (
+        <CommingSoon
+          name="MEtime Mandala"
+          desc="A platform working on digitizing trauma-informed, participatory programmed and print media that has been tested with +15,000 people in SA, India, the Philippines and Sudan since 2011. Being made in conjunction with EduSOIL Pvt. Ltd. a group of mental wellness experts situated in the UK, the platform is a culturally responsive, customizable and gamified  platform, empowering children and teachers to learn and lead 10 min mental health breaks."
+        />
+      )
 
     default:
       return <h1>404 Not Found</h1>
